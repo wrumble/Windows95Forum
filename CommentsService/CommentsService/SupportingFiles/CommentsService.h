f2c542befe672fd8ac81644d54035a14bbbc3060
wrumble <rumblewayne@gmail.com>
2021-09-23 16:26:26 +0100

    create services

---
+++ CommentsService/CommentsService/SupportingFiles/CommentsService.h
@@ -0,0 +1,18 @@
+//
+//  CommentsService.h
+//  CommentsService
+//
+//  Created by Wayne Rumble on 23/09/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CommentsService.
+FOUNDATION_EXPORT double CommentsServiceVersionNumber;
+
+//! Project version string for CommentsService.
+FOUNDATION_EXPORT const unsigned char CommentsServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CommentsService/PublicHeader.h>
+
+
---
+++ UsersService/UsersService/UsersService.swift
@@ -0,0 +1,19 @@
+import Combine
+
+import ForumClient
+
+public protocol UsersServiceProtocol {
+  func getUsers() -> AnyPublisher<[User], APIError>
+}
+
+public class UsersService: UsersServiceProtocol {
+  private let client: ForumClientProtocol
+
+  public required init(client: ForumClientProtocol = ForumClient()) {
+    self.client = client
+  }
+
+  public func getUsers() -> AnyPublisher<[User], APIError> {
+    return client.execute(.users)
+  }
+}
